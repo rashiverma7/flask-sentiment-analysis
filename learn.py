6bcff53696bef22b950a8505e53aa22f3f60b470
Aryan Raj <aryan5416@gmail.com>
2019-04-10 04:50:06 +0530

    bug fixes

--- learn.py
+++ learn.py
@@ -146,7 +146,7 @@ def loadData(product_id):
 
     def modelEvaluation(predictions):
         print ("\nAccuracy on validation set: {:.4f}".format(accuracy_score(y_test, predictions)))
-        print("\nAUC score : {:.4f}".format(roc_auc_score(y_test, predictions)))
+        #print("\nAUC score : {:.4f}".format(roc_auc_score(y_test, predictions)))
         print("\nClassification report : \n", metrics.classification_report(y_test, predictions))
         print("\nConfusion Matrix : \n", metrics.confusion_matrix(y_test, predictions))
 
